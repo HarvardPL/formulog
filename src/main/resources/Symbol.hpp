dededbf2e3c3c32802efb1c9a7796434cd09ad83
Aaron Bembenek <aaron.bembenek@gmail.com>
2020-01-06 09:55:38 -0500

    Made Symbol printable.

--- src/main/resources/Symbol.hpp
+++ src/main/resources/Symbol.hpp
@@ -2,7 +2,7 @@
 #define _SYMBOL_HPP__
 
 #include <cstdlib>
-#include <string>
+#include <iostream>
 
 namespace flg {
 
@@ -12,8 +12,19 @@ enum class Symbol {
   boxed_fp32,
   boxed_fp64,
   boxed_string,
-  // >>> INSERT SYMBOLS <<<
+  // |> INSERT 0 <|
 };
+
+std::ostream& operator<<(std::ostream& out, const Symbol sym) {
+	switch (sym) {
+	case Symbol::boxed_i32: return out << "boxed_i32";
+	case Symbol::boxed_i64: return out << "boxed_i64";
+	case Symbol::boxed_fp32: return out << "boxed_fp32";
+	case Symbol::boxed_fp64: return out << "boxed_fp64";
+	case Symbol::boxed_string: return out << "boxed_string";
+  // |> INSERT 1 <|
+	}
+}
 
 } // namespace flg
 
