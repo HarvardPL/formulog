f66a6a956fef0eb8f63e8c93e4a67d262a7493e2
Aaron Bembenek <aaron.bembenek@gmail.com>
2020-01-17 13:28:09 -0500

    Wrote is_sat function.

--- src/main/resources/smt.hpp
+++ src/main/resources/smt.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstdlib>
 #include <iostream>
 #include <boost/process.hpp>
 
@@ -17,32 +18,47 @@ auto declarations = R"_(
 enum class SmtStatus { sat, unsat, unknown };
 
 struct SmtShim {
-
   SmtShim();
-
   SmtStatus is_sat(const term_ptr& assertion);
 
   private:
-  bp::opstream in;
-  bp::ipstream out;
-  bp::ipstream err;
+  bp::opstream z3_in;
+  bp::ipstream z3_out;
   bp::child z3;
 
 	void serialize(const term_ptr& assertion, ostream& out);
 };
 
 SmtShim::SmtShim() :
-  z3("z3 -in", bp::std_in < in, bp::std_out > out, bp::std_err > err) {
-  in << declarations << endl;
-  in.flush();
+  z3("z3 -in", bp::std_in < z3_in, (bp::std_out & bp::std_err) > z3_out) {
+  z3_in << declarations << endl;
+  z3_in.flush();
 }
 
 SmtStatus SmtShim::is_sat(const term_ptr& assertion) {
-  return SmtStatus::sat;
+  z3_in << "(assert ";
+  serialize(assertion, z3_in);
+  z3_in << ")" << endl;
+  z3_in << "(check-sat)" << endl;
+  z3_in.flush();
+  string line;
+  getline(z3_out, line);
+  if (line == "sat") { 
+    return SmtStatus::sat; 
+  } else if (line == "unsat") {
+    return SmtStatus::unsat;
+  } else if (line == "unknown") {
+    return SmtStatus::unknown;
+  } else {
+    cerr << "Unexpected Z3 response:" << endl;
+    cerr << line << endl;
+    abort();
+  }
+  __builtin_unreachable();
 }
 
 void SmtShim::serialize(const term_ptr& assertion, ostream& out) {
-  out << "(true)" << endl;	
+  out << "true" << endl;	
 }
 
 thread_local SmtShim smt_shim;
