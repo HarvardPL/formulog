23e6f39a1733c5ec15c7a79cd393bd87bb546ea5
Aaron Bembenek <aaron.bembenek@gmail.com>
2020-01-30 13:27:48 -0500

    Cleaned up unused imports.

--- src/main/java/edu/harvard/seas/pl/formulog/codegen/CodeGen.java
+++ src/main/java/edu/harvard/seas/pl/formulog/codegen/CodeGen.java
@@ -22,16 +22,11 @@ package edu.harvard.seas.pl.formulog.codegen;
 
 import java.io.File;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
