83e4286258d3be203570a2d2ea3b7adb148c11e0
Aaron Bembenek <aaron.bembenek@gmail.com>
2020-02-15 11:11:11 -0500

    Compute min index.

---
+++ src/main/java/edu/harvard/seas/pl/formulog/db/MinIndex.java
@@ -0,0 +1,69 @@
+package edu.harvard.seas.pl.formulog.db;
+
+/*-
+ * #%L
+ * FormuLog
+ * %%
+ * Copyright (C) 2018 - 2020 President and Fellows of Harvard College
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public final class MinIndex {
+
+	private MinIndex() {
+		throw new AssertionError("impossible");
+	}
+	
+	public static <T> Iterable<Iterable<T>> compute(Set<Set<T>> searches) {
+		MinChainCover<Set<T>> mcc = new MinChainCover<>((x, y) -> y.containsAll(x));
+		Iterable<Iterable<Set<T>>> chains = mcc.compute(searches); 
+		List<Iterable<T>> idxs = new ArrayList<>();
+		for (Iterable<Set<T>> chain : chains) {
+			idxs.add(computeIndex(chain));
+		}
+		return idxs;
+	}
+	
+	private static <T> Iterable<T> computeIndex(Iterable<Set<T>> chain) {
+		List<T> index = new ArrayList<>();
+		Set<T> prev = Collections.emptySet();
+		for (Set<T> s : chain) {
+			for (T elt : s) {
+				if (!prev.contains(elt)) {
+					index.add(elt);
+				}
+			}
+			prev = s;
+		}
+		return index;
+	}
+	
+	public static void main(String[] args) {
+		Set<String> s1 = new HashSet<>(Arrays.asList("x"));
+		Set<String> s2 = new HashSet<>(Arrays.asList("x", "y"));
+		Set<String> s3 = new HashSet<>(Arrays.asList("x", "z"));
+		Set<String> s4 = new HashSet<>(Arrays.asList("x", "y", "z"));
+		Set<Set<String>> searches = new HashSet<>(Arrays.asList(s1, s2, s3, s4));
+		System.out.println(MinIndex.compute(searches));
+	}
+
+}
