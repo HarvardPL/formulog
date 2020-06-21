c37d77356cffe95bd3a3af6218e6247f9416fe61
Rose Hong <rosemhong@gmail.com>
2020-06-21 13:32:33 -0700

    Add new tests for InflationaryEvaluationTest.java and SemiInflationaryEvaluationTest.java

--- src/test/java/edu/harvard/seas/pl/formulog/eval/InflationaryEvaluationTest.java
+++ src/test/java/edu/harvard/seas/pl/formulog/eval/InflationaryEvaluationTest.java
@@ -34,4 +34,10 @@ public class InflationaryEvaluationTest extends AbstractEvaluationTest {
     @Test
     public void test002() { test("inflationary/test002_ok.flg"); }
 
+    @Test
+    public void test003() { test("inflationary/test003_ok.flg"); }
+
+    @Test
+    public void test004() { test("inflationary/test004_ok.flg"); }
+
 }
