ab611155a63f9100c84dee0b44be24b0917ce5a5
Aaron Bembenek <aaron.bembenek@gmail.com>
2020-07-21 12:36:25 -0400

    Added comments on StratumCodeGen.

--- src/main/java/edu/harvard/seas/pl/formulog/codegen/StratumCodeGen.java
+++ src/main/java/edu/harvard/seas/pl/formulog/codegen/StratumCodeGen.java
@@ -20,7 +20,6 @@ package edu.harvard.seas.pl.formulog.codegen;
  * #L%
  */
 
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,19 +30,38 @@ import edu.harvard.seas.pl.formulog.symbols.RelationSymbol;
 import edu.harvard.seas.pl.formulog.util.Pair;
 import edu.harvard.seas.pl.formulog.validating.Stratum;
 
+/**
+ * This class is used to take a stratum (from a stratified Formulog program) and
+ * generate C++ code that evaluates it.
+ */
 public class StratumCodeGen {
 
 	private final CodeGenContext ctx;
+	/**
+	 * This variable is used in the C++ code to track whether a new tuple has been
+	 * derived; if so, we need to keep evaluating, since we've yet to reach a fix
+	 * point.
+	 */
 	private static final CppVar changed = CppVar.mk("changed");
 
 	public StratumCodeGen(CodeGenContext ctx) {
 		this.ctx = ctx;
 	}
 
+	/**
+	 * Take a stratum (from a stratified Formulog program) and generate C++ code
+	 * that evaluates it.
+	 * 
+	 * @param stratum
+	 * @return The C++ representation of the stratum evaluation
+	 */
 	public CppStmt gen(Stratum stratum) {
 		List<CppStmt> stmts = new ArrayList<>();
 		stmts.add(CppDecl.mk("changed", CppConst.mkFalse()));
 		RuleCodeGen rcg = new RuleCodeGen(ctx);
+		// Split rules into first-round rules (which refer to predicates from only lower
+		// strata, and so only need to be executed once) and later-round rules, which
+		// need to be executed in a loop until a fix point is reached.
 		List<IndexedRule> firstRoundRules = new ArrayList<>();
 		List<IndexedRule> laterRoundRules = new ArrayList<>();
 		for (RelationSymbol sym : stratum.getPredicateSyms()) {
@@ -60,17 +78,36 @@ public class StratumCodeGen {
 		return CppSeq.mk(stmts);
 	}
 
+	/**
+	 * Generate C++ code for evaluating the first-round rules. These are rules that
+	 * refer to predicates from only lower strata, and so only need to be evaluated
+	 * once.
+	 * 
+	 * @param stratum
+	 * @param rules
+	 * @param rcg
+	 * @return The C++ code for evaluating the first-round rules
+	 */
 	private CppStmt genFirstRound(Stratum stratum, Iterable<IndexedRule> rules, RuleCodeGen rcg) {
 		List<CppStmt> stmts = new ArrayList<>();
 		stmts.add(genRules(rules, rcg, true));
 		for (RelationSymbol sym : stratum.getPredicateSyms()) {
 			Relation rel = ctx.lookupRelation(sym);
 			Relation delta = ctx.lookupRelation(new DeltaSymbol(sym));
+			// Insert anything we've derived into the Delta DB
 			stmts.add(delta.mkInsertAll(CppUnop.mkDeref(rel)).toStmt());
 		}
 		return CppSeq.mk(stmts);
 	}
 
+	/**
+	 * Take a bunch of rules and generate code for running each of those rules once.
+	 * 
+	 * @param rules
+	 * @param rcg
+	 * @param isFirstRound
+	 * @return The C++ code for running the rules
+	 */
 	private CppStmt genRules(Iterable<IndexedRule> rules, RuleCodeGen rcg, boolean isFirstRound) {
 		List<CppStmt> stmts = new ArrayList<>();
 		for (IndexedRule r : rules) {
@@ -79,11 +116,30 @@ public class StratumCodeGen {
 		return CppSeq.mk(stmts);
 	}
 
+	/**
+	 * Take a rule and generate code for running that rule once.
+	 * 
+	 * @param r
+	 * @param rcg
+	 * @param firstRound
+	 * @return The C++ code for running the rule
+	 */
 	private CppStmt genRule(IndexedRule r, RuleCodeGen rcg, boolean firstRound) {
 		Pair<CppStmt, CppExpr> p = rcg.gen(r, firstRound);
+		// Make sure the changed variable is updated to true if the rule derived
+		// something new
 		return CppSeq.mk(p.fst(), CppBinop.mkOrUpdate(changed, p.snd()).toStmt());
 	}
 
+	/**
+	 * Generate a loop for running the later-round rules until a fix point is
+	 * reached.
+	 * 
+	 * @param stratum
+	 * @param rules
+	 * @param rcg
+	 * @return The C++ code for the loop
+	 */
 	private CppStmt genLoop(Stratum stratum, Iterable<IndexedRule> rules, RuleCodeGen rcg) {
 		CppStmt setFalse = CppBinop.mkAssign(changed, CppConst.mkFalse()).toStmt();
 		CppStmt evalCode = genRules(rules, rcg, false);
@@ -91,9 +147,20 @@ public class StratumCodeGen {
 		return CppWhile.mk(changed, CppSeq.mk(setFalse, evalCode, updateCode));
 	}
 
+	/**
+	 * Generate C++ code for updating the fact databases after an iteration of the
+	 * evaluation loop.
+	 * 
+	 * @param stratum
+	 * @return The C++ code for updating the databases
+	 */
 	private CppStmt genUpdateCode(Stratum stratum) {
 		List<CppStmt> stmts = new ArrayList<>();
 		for (RelationSymbol sym : stratum.getPredicateSyms()) {
+			// The generated C++ code for rule evaluation puts newly derived stuff in
+			// newRel, so here we add that stuff to the cumulative DB for that relation and
+			// set the Delta DB to be newRel (so that the new tuples are used as the "delta"
+			// tuples in the next iteration of evaluation).
 			Relation rel = ctx.lookupRelation(sym);
 			Relation delta = ctx.lookupRelation(new DeltaSymbol(sym));
 			Relation newRel = ctx.lookupRelation(new NewSymbol(sym));
