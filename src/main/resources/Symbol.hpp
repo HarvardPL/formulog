fd515cffdf331f327a3b9ed6e47f2930e1c0f784
Eric Zhang <ekzhang1@gmail.com>
2020-07-25 05:21:04 +0000

    Move Symbol.cpp into its own compilation unit

--- src/main/resources/Symbol.hpp
+++ src/main/resources/Symbol.hpp
@@ -19,46 +19,16 @@ enum class Symbol {
 /* INSERT 0 */
 };
 
-ostream& operator<<(ostream& out, Symbol sym) {
-  switch (sym) {
-    case Symbol::boxed_bool: return out << "boxed_bool";
-    case Symbol::boxed_i32: return out << "boxed_i32";
-    case Symbol::boxed_i64: return out << "boxed_i64";
-    case Symbol::boxed_fp32: return out << "boxed_fp32";
-    case Symbol::boxed_fp64: return out << "boxed_fp64";
-    case Symbol::boxed_string: return out << "boxed_string";
-/* INSERT 1 */
-  }
-  __builtin_unreachable();
-}
+extern map<string, Symbol> symbol_table;
 
-map<string, Symbol> symbol_table;
-
-void initialize_symbols() {
-/* INSERT 2 */
-}
-
-Symbol lookup_symbol(const string& name) {
-  auto it = symbol_table.find(name);
-  if (it != symbol_table.end()) {
-    return it->second;
-  }
-  cerr << "Unrecognized symbol: " << name << endl;
-  abort();
-}
-
-Symbol lookup_tuple_symbol(size_t arity) {
-  switch (arity) {
-/* INSERT 3 */
-    default:
-    cerr << "Unrecognized tuple arity: " << arity << endl;
-    abort();
-  }
-}
+ostream& operator<<(ostream& out, Symbol sym);
+void initialize_symbols();
+Symbol lookup_symbol(const string& name);
+Symbol lookup_tuple_symbol(size_t arity);
 
 constexpr size_t symbol_arity(Symbol sym) {
   switch (sym) {
-/* INSERT 4 */
+/* INSERT 1 */
     default: abort();
   }
 }
