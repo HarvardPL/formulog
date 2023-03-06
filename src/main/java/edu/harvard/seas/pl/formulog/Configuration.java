afa3a398226cfb339c5b3ded814361451d0dc096
Aaron Bembenek <aaron.bembenek@gmail.com>
2023-03-06 09:00:09 -0500

    Add stats for amount of work done.

--- src/main/java/edu/harvard/seas/pl/formulog/Configuration.java
+++ src/main/java/edu/harvard/seas/pl/formulog/Configuration.java
@@ -168,6 +168,9 @@ public final class Configuration {
     public static final boolean inlineInRules = propIsSet("inlineInRules", true);
 
     public static final boolean eagerSemiNaive = propIsSet("eagerSemiNaive");
+    
+    public static final boolean recordWork = propIsSet("recordWork");
+    public static final AtomicLong work = new AtomicLong();
 
     static {
         if (recordFuncDiagnostics) {
