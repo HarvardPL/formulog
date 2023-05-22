ab2ac9173d7a0de53583553d37e876e3a2b46c5c
Aaron Bembenek <aaron.bembenek@gmail.com>
2023-05-22 17:42:17 -0400

    Codegen: do not split on SMT calls by default.

--- src/main/java/edu/harvard/seas/pl/formulog/Configuration.java
+++ src/main/java/edu/harvard/seas/pl/formulog/Configuration.java
@@ -171,7 +171,7 @@ public final class Configuration {
 	public static final boolean inlineInRules = propIsSet("inlineInRules", true);
 
 	public static final boolean eagerSemiNaive = propIsSet("eagerSemiNaive");
-	public static final boolean codegenSplitOnSmt = propIsSet("codegenSplitOnSmt", true);
+	public static final boolean codegenSplitOnSmt = propIsSet("codegenSplitOnSmt");
 
 	public static final boolean useHashDbFilter = propIsSet("useHashDbFilter");
 
